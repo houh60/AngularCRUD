34c6a5dab8f94b5dffd97bae607d0a3da8dfe3e8
William Hou <williamhou38@yahoo.com>
2024-08-04 20:30:45 -0500

    new changes

--- src/app/employees/create-employees/create-employees.component.ts
+++ src/app/employees/create-employees/create-employees.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { Department } from '../../models/department.model';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 import { Employee } from '../../models/employee.model';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
+import * as data from '../../shared/data';
 
 @Component({
   selector: 'app-create-employees',
@@ -28,22 +30,20 @@ export class CreateEmployeesComponent {
     photoPath: null,
   }
 
-  departments: Department[] = [
-    { id: 1, name: 'Help Desk' },
-    { id: 2, name: 'HR' },
-    { id: 3, name: 'IT' },
-    { id: 4, name: 'Payroll' },
-    { id: 5, name: 'Admin' },
-  ];
+  departments: Department[] = data.departments;
 
-  constructor() {
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {
     this.datepickerConfig = Object.assign({}, {
       containerClass: 'theme-dark-blue',
       showWeekNumbers: false
     })
   }
 
-  saveEmployee(newEmployee: Employee) {
-    console.log("newEmployee: ", newEmployee);
+  saveEmployee() {
+    this.employeeService.save(this.employee);
+    this.router.navigate(['/list']);
   }
 }
---
+++ src/app/shared/data.ts
@@ -0,0 +1,9 @@
+import { Department } from "../models/department.model";
+
+export const departments: Department[] = [
+  { id: 1, name: 'Help Desk' },
+  { id: 2, name: 'HR' },
+  { id: 3, name: 'IT' },
+  { id: 4, name: 'Payroll' },
+  { id: 5, name: 'Admin' },
+];
