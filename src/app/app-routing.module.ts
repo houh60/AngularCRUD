3e1a15d4fb7dad7ce726f65cf8ad578440800318
William Hou <williamhou38@yahoo.com>
2024-08-12 20:31:23 -0500

    new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,11 +9,31 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { EmployeeDetailsGuardService } from './employees/employee-details-guard.service';
 
 const routes: Routes = [
-  { path: 'list', component: ListEmployeesComponent, resolve: { employeeList: EmployeeListResolverService } },
-  { path: 'employees/:id', component: EmployeeDetailsComponent, canActivate: [EmployeeDetailsGuardService], resolve: { employeeList: EmployeeListResolverService } },
-  { path: 'edit/:id', component: CreateEmployeesComponent, canDeactivate: [CanDeactivateGuardService] },
-  { path: '', redirectTo: '/list', pathMatch: 'full' },
-  { path: 'notfound', component: PageNotFoundComponent },
+  {
+    path: 'list',
+    component: ListEmployeesComponent,
+    resolve: { employeeList: EmployeeListResolverService }
+  },
+  {
+    path: 'employees/:id',
+    component: EmployeeDetailsComponent,
+    canActivate: [EmployeeDetailsGuardService],
+    resolve: { employeeList: EmployeeListResolverService }
+  },
+  {
+    path: 'edit/:id',
+    component: CreateEmployeesComponent,
+    canDeactivate: [CanDeactivateGuardService]
+  },
+  {
+    path: '',
+    redirectTo: '/list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'notfound',
+    component: PageNotFoundComponent
+  },
 ];
 
 @NgModule({
